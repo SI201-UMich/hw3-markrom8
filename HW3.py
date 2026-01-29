68f74cf9dff433348c64424072c1b9b73ad8b190
Mark Roman <markroman@Marks-MacBook-Air-57.local>
2026-01-29 15:32:30 -0500

    Completed init method

--- HW3.py
+++ HW3.py
@@ -33,7 +33,11 @@ class CouponDispenser:
             coupon_cards (list[str]): list of possible coupons users can receive.
         """
         # TODO: Implement per instructions
-        pass
+        self.coupon_cards = coupon_cards
+        
+        self.customer_roster = []
+
+        self.issued_indices = []
 
     def __str__(self):
         """
